a52c66339a773902357c05ddbd57d5e411afd5a0
laisdenegri <lais_ribeiro@beirario.com.br>
2022-08-03 17:04:38 -0300

    Created Deposit, Account, Statement and WithDraw

--- src/index.js
+++ src/index.js
@@ -1,9 +1,118 @@
 const { response, request } = require('express');
 const express = require('express');
 
+const { v4: uuidv4 } = require("uuid")
+
 const app = express();
 
 app.use(express.json());
+
+const customers = [];
+
+/**
+ * CPF = String
+ * Name = String
+ * Id = uuid
+ * Statement []
+ */
+
+// Middleware - Defini-se um middleware por receber 3 parâmetros (request, response, next)
+// É o next que define se o middleware vai para frente ou não
+function verifyIfExistAccountCPF(request, response, next) {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) return response.status(400).json({ error: "Customer not found" })
+
+    request.customer = customer;
+    return next();
+
+}
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit"){
+             return acc + operation.amount
+        }else{
+            return acc - operation.amount
+        }
+    }, 0)   //Valor que inicia o reduce
+
+    return balance;
+
+}
+
+
+app.post("/account", (request, response) => {
+    const {cpf, name} = request.body;
+
+    //some, vai retornar um true or false para a verificação
+    const customersAlreadyExist = customers.some((customer) => {
+        customer.cpf === cpf
+    });
+
+    if(customersAlreadyExist) return response.status(400).json({ error: "Customer already exist!" });
+    
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send();
+
+})
+
+// Se preciso que todas as requisições abaixo verifiquem se existe o cfp
+// app.use(verifyIfExistAccountCPF);
+
+app.get("/statement/:cpf", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer.statement);
+})
+
+app.post("/deposit/:cpf", verifyIfExistAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+app.post("/withdraw/:cpf", verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) return response.status(400).json({ error: "Insufficient funds" });
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+
+//localhost:3333
+app.listen(3333);
 
 
 /**
@@ -29,37 +138,34 @@ app.use(express.json());
 // })
 
 //Método simples direto pelo navegador, acessamos a rota e ela já retorna o resultado
-app.get("/courses", (request, response) => {
-    const query = request.query;
-    console.log(query);
+// app.get("/courses", (request, response) => {
+//     const query = request.query;
+//     console.log(query);
 
-    // /courses?page=1&order=asc
+//     // /courses?page=1&order=asc
 
-    return response.json(["Curso 1", "Curso 2", "Curso 3"])
-})
+//     return response.json(["Curso 1", "Curso 2", "Curso 3"])
+// })
 
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
+// app.post("/courses", (request, response) => {
+//     const body = request.body;
+//     console.log(body);
 
-    return response.json(["Curso 1", "Curso 2", "Curso 3"])
-})
+//     return response.json(["Curso 1", "Curso 2", "Curso 3"])
+// })
 
-app.put("/courses/:id", (request, response) => {
-    const { id } = request.params;
-    console.log(id)
-    return response.json(["Curso 6", "Curso 2", "Curso 3"])
-})
+// app.put("/courses/:id", (request, response) => {
+//     const { id } = request.params;
+//     console.log(id)
+//     return response.json(["Curso 6", "Curso 2", "Curso 3"])
+// })
 
-app.patch("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 7", "Curso 3"])
-})
+// app.patch("/courses/:id", (request, response) => {
+//     return response.json(["Curso 6", "Curso 7", "Curso 3"])
+// })
 
-app.delete("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 7"])
-})
+// app.delete("/courses/:id", (request, response) => {
+//     return response.json(["Curso 6", "Curso 7"])
+// })
 
 
-
-//localhost:3333
-app.listen(3333);
